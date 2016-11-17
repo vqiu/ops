9c0b060777e1343a64b41e5824bf77b648ad63b0
shuhui.qiu <vqiu@users.noreply.github.com>
2016-11-17 23:26:11 +0800

    Update and rename curls.sh to user-define.sh

--- alias/user-define.sh
+++ alias/user-define.sh
@@ -1,4 +1,6 @@
-# /etc/profile.d/curls.sh
+# /etc/profile.d/user-define.sh
+
+# curl 命令增加版
 alias curld='curl -s -o /dev/null -w \
 "
               请求时间:  '$(date +%y-%m-%dT%T+08:00)'
@@ -15,5 +17,20 @@ alias curld='curl -s -o /dev/null -w \
                          --------------
             消耗总时长:  %{time_total} s \n
 "'
+
+# curl 命令简洁版
 alias curlb='curl -s -o /dev/null -w \
 "'$(date +%y-%m-%dT%T+08:00)' %{http_code} %{time_namelookup} %{time_connect} %{time_appconnect} %{time_pretransfer} %{time_redirect} %{time_starttransfer} %{size_request} %{size_download} %{speed_download} %{time_total}\n"'
+
+# 网络连接最大数
+alias conn_max="netstat -na|awk '\$5 ~ /[0-9]+:[0-9]+/ {print \$5}' |awk -F ':' -- '{print \$1}' |sort -n|uniq -c |sort -rn|tail -n 15"
+
+# TCP连接状态数据
+alias conn_ts="netstat -n | awk '/^tcp/ {++S[\$NF]} END {for(a in S) print a, S[a]}'"
+
+mcd() { mkdir -p "\$1"; cd "\$1";}
+alias ..="cd .."
+alias ...="cd ../../"
+alias listen="lsof -P -i -n"
+alias ll="ls -al --color=auto"
+alias randPass="openssl rand -base64 32"
